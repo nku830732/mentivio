a32f2dc52d388fe1b54cdfd46c313c0d15992c57
nitish830732 <nitiskkumar857791@gmail.com>
2022-01-04 15:09:30 +0530

    first change

--- course/templatetags/course_custom_tags.py
+++ course/templatetags/course_custom_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 # 100 -> 10% --> mrp  - ( mrp * discount * 0.01 ) = selprice
 @register.simple_tag
 def cal_sellprice(price , discount):
-    if discount is None or discount is 0:
+    if discount== 0:
         return price
     sellprice = price
     sellprice = price - ( price * discount * 0.01 )
