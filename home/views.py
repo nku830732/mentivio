e57eca7bb82f887606ed104e0b83dcc9f69b0624
nku830732 <nitiskkumar857791@gmail.com>
2022-01-06 16:18:32 +0530

    again

--- home/views.py
+++ home/views.py
@@ -6,12 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from home.models import Contact, Newslatter_Subscriber
 # Create your views here.
-def home(request):
-    user = request.user
-    context = {
-        'user': user
-    }   
-    return render(request,'EduGuider/admin/index.html', context)
+def home(request): 
+    return render(request,'EduGuider/admin/index.html')
 def about(request):
        
     return render(request,'EduGuider/about-1.html' )
