2eb0d07834daa22fb794d3fb7429d9422f2b7102
nku830732 <nitiskkumar857791@gmail.com>
2022-01-07 20:35:13 +0530

    new

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,26 @@
-from django.http.response import HttpResponse
 from django.shortcuts import redirect, render,HttpResponse
+from django.http import request
 from blog.models import Career_Oppotunitie, Common_strategie,Exam,Career_Oppotunitie_Comment,Oppotunitie_Comment_Reply,strategyQuesition, blogAds
 from django.contrib import messages
 from home.models import Newslatter_Subscriber
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def strategies(request):
-    if request.method=="POST":
-        email_subscriber= request.POST['email_subscriber']
-        print('email_subscriber')
-        Newslatter =Newslatter_Subscriber(email_subscriber=email_subscriber)
-        Newslatter.save()
-        messages.success(request, "Subscribed")
+    
     allstrategies = Common_strategie.objects.all()
     ads = blogAds.objects.all()
     print(allstrategies)
     context = {'allstrategies': allstrategies,'ads':ads}
     return render(request,'EduGuider/strategies.html', context)
 def strategiesPost(request, slug):
-    if request.method=="POST":
-        email_subscriber= request.POST['email_subscriber']
-        print('email_subscriber')
-        Newslatter =Newslatter_Subscriber(email_subscriber=email_subscriber)
-        Newslatter.save()
-        messages.success(request, "Subscribed")
+    
     strategie = Common_strategie.objects.filter(slug=slug).first()
     ads = blogAds.objects.all()
     comments = strategyQuesition.objects.filter(post=strategie)
     context = {'strategie' : strategie,'ads':ads, 'comments':comments}
     return render(request,'EduGuider/strategydetails.html', context)
 def opportunities(request):
-    if request.method=="POST":
-        email_subscriber= request.POST['email_subscriber']
-        print('email_subscriber')
-        Newslatter =Newslatter_Subscriber(email_subscriber=email_subscriber)
-        Newslatter.save()
-        messages.success(request, "Subscribed")
+   
     allopportunities = Career_Oppotunitie.objects.all()
     ads = blogAds.objects.all()
     print(allopportunities)
