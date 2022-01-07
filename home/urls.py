18bd600c27ba1f932e010433b989e813775fc022
nku830732 <nitiskkumar857791@gmail.com>
2022-01-07 09:41:41 +0530

    m

--- home/urls.py
+++ home/urls.py
@@ -5,14 +5,14 @@ from testseries.views import verifyPaymenttest
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about', views.about, name="about"),
-    path('faqs', views.faqs, name="faqs"),
-    path("contact", views.contact, name='contact'),
-    path('membership', views.membership, name="membership"),
+    path('about/', views.about, name="about"),
+    path('faqs/', views.faqs, name="faqs"),
+    path("contact/", views.contact, name='contact'),
+    path('membership/', views.membership, name="membership"),
     path('accounts/login', views.handleLogin, name="login"),
     path('accounts/signup', views.handleSignup, name="register"),
-    path('forgetpass', views.forgetpass, name="forgetpass"),
+    path('forgetpass/', views.forgetpass, name="forgetpass"),
     path('success/url/', views.success, name="success"),
-    path('verify_payment', verifyPayment , name = 'verify_payment'),
-    path('verify_paymenttest', verifyPaymenttest , name = 'verify_paymenttest'),
+    path('verify_payment/', verifyPayment , name = 'verify_payment'),
+    path('verify_paymenttest/', verifyPaymenttest , name = 'verify_paymenttest'),
 ]
