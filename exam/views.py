4426f504b77a13f0de9e8c0670d6c147e0ccb162
nku830732 <nitiskkumar857791@gmail.com>
2022-01-24 12:14:28 +0530

    new version

--- exam/views.py
+++ exam/views.py
@@ -9,18 +9,25 @@ from django.contrib import messages
 # Create your views here.
 def exams(request):
        cat=request.GET.get('category')
+       allcats = Category.objects.all()
        if cat is not None:
            ex=Category.objects.get(name= cat)   
            allexams=Exam.objects.filter(category=ex)
-       else:
-          allexams = Exam.objects.all()
-       allcats = Category.objects.all()
-        
-       context = {
+           context = {
               'allexams': allexams,
                'allcats': allcats,
                'ex':ex
        }
+       else:
+          allexams = Exam.objects.all()
+          context = {
+              'allexams': allexams,
+               'allcats': allcats,
+               
+       }
+       
+        
+       
        return render(request,'Eduguider/exams.html', context)
 def examdetails(request, slug_exam):
        exam = Exam.objects.filter(slug_exam=slug_exam).first() 
